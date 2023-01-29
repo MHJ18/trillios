a8c68b30f05ada8509d0844765b257a3b517a9c6
Hassaan1814 <hassaanjaved82@triconv.com>
2023-01-29 21:10:38 +0500

    new project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./navigation.scss";
+const Navbar = () => {
+  return (
+    <div className="navbar__container">
+      <nav className="navbar__item">
+        <img src="favicon.png" alt="" />
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
